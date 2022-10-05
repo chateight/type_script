542a9696aa7d12ffa597a9093a638439ed0f5306
rusami <isehara.jp@coderdojo.com>
2022-10-05 20:35:31 +0900

    # compiled JS file is added

---
+++ ts_basic.js
@@ -0,0 +1,5 @@
+function greeter_ts(person) {
+    return "Hello, " + person;
+}
+var user_ts = "John";
+console.log(greeter_ts(user_ts));
--- ts_basic.ts
+++ ts_basic.ts
@@ -1,3 +1,6 @@
+// .js is a compiled file
+// tsc ts_basic.ts
+
 function greeter_ts(person: string) {
   return "Hello, " + person;
 }
