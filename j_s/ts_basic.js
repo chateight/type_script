5982768cb2e87ddb85fbb900c540c8828512a0cc
rusami <isehara.jp@coderdojo.com>
2022-10-06 13:27:25 +0900

    # to use "tsconfig.json"

--- j_s/ts_basic.js
+++ j_s/ts_basic.js
@@ -1,7 +1,8 @@
+"use strict";
 // .js is a compiled file
 // $ tsc ts_basic.ts
 function greeter_ts(person) {
     return "Hello, " + person;
 }
-var user_ts = "John";
+let user_ts = "John";
 console.log(greeter_ts(user_ts));
